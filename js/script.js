09452c2cd86e68c8c62052e35e7ec17a4c45d809
leoanrdo77pk <suporte.purofut@gmail.com>
2025-04-24 19:04:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+function toggleMenu() {
+  const menu = document.getElementById("menuLateral");
+  const content = document.querySelector(".content");
+
+  if (menu.style.width === "250px") {
+    menu.style.width = "0";
+    content.style.marginLeft = "0";
+  } else {
+    menu.style.width = "250px";
+    content.style.marginLeft = "250px";
+  }
+}
+
+document.getElementById("searchInput").addEventListener("input", function() {
+  const filter = this.value.toLowerCase();
+  const cards = document.querySelectorAll("#catalogo .card");
+
+  cards.forEach(card => {
+    const title = card.querySelector("a").textContent.toLowerCase();
+    card.style.display = title.includes(filter) ? "flex" : "none";
+  });
+});
 
