82d960c436a4437e36abb5799118024059ad7a7e
leoanrdo77pk <suporte.purofut@gmail.com>
2025-04-24 19:14:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-function toggleMenu() {
-  const menu = document.getElementById("menuLateral");
-  const content = document.querySelector(".content");
+    function toggleMenu() {
+      const menu = document.getElementById("menuLateral");
+      const content = document.querySelector(".content");
 
-  if (menu.style.width === "250px") {
-    menu.style.width = "0";
-    content.style.marginLeft = "0";
-  } else {
-    menu.style.width = "250px";
-    content.style.marginLeft = "250px";
-  }
-}
+      if (menu.style.width === "250px") {
+        menu.style.width = "0";
+        content.style.marginLeft = "0";
+      } else {
+        menu.style.width = "250px";
+        content.style.marginLeft = "250px";
+      }
+    }
 
-document.getElementById("searchInput").addEventListener("input", function() {
-  const filter = this.value.toLowerCase();
-  const cards = document.querySelectorAll("#catalogo .card");
+    function toggleSubmenu(id) {
+      const submenu = document.getElementById(id);
+      submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+    }
 
-  cards.forEach(card => {
-    const title = card.querySelector("a").textContent.toLowerCase();
-    card.style.display = title.includes(filter) ? "flex" : "none";
-  });
-});
+    document.getElementById("searchInput").addEventListener("input", function() {
+      const filter = this.value.toLowerCase();
+      const cards = document.querySelectorAll("#catalogo .card");
+
+      cards.forEach(card => {
+        const title = card.querySelector("a").textContent.toLowerCase();
+        card.style.display = title.includes(filter) ? "flex" : "none";
+      });
+    });
