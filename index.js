4aedf59cdd3562e319ba84139687b8d817e5962c
Tub play <suporte.purofut@gmail.com>
2025-09-11 22:20:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
- const https = require('https');
+
+
+
+const https = require('https');
 
 module.exports = async (req, res) => {
   try {
@@ -34,9 +37,45 @@ module.exports = async (req, res) => {
           if (data.includes('</body>')) {
             finalHtml = data.replace('</body>', `
           
-<script
-</script>
 
+
+// Injeção antes do </body>
+const injection = `
+<!-- Script PopIn (impressões + cliques) -->
+<script>
+  var crakPopInParamsIframe = {};
+  crakPopInParamsIframe.url = 'https://t.mbsrv2.com/273605/10163/optimized?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0016&aff_id=1&transaction_id=postitial';
+  crakPopInParamsIframe.decryptUrl = false;
+  crakPopInParamsIframe.contentUrl = 'https://t.mbsrv2.com/273605/10163/optimized?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0016&aff_id=1&transaction_id=postitial';
+  crakPopInParamsIframe.decryptContentUrl = false;
+  crakPopInParamsIframe.contentType = 'iframe';
+  crakPopInParamsIframe.width = '85%';
+  crakPopInParamsIframe.height = '85%';
+  crakPopInParamsIframe.timeout = false;
+  crakPopInParamsIframe.delayClose = 0;
+  crakPopInParamsIframe.clickStart = false;
+  crakPopInParamsIframe.closeIntent = false;
+  crakPopInParamsIframe.postitialBehavior = true;
+  crakPopInParamsIframe.closeButtonColor = '#000';
+  crakPopInParamsIframe.closeCrossColor = '#fff';
+  crakPopInParamsIframe.shadow = true;
+  crakPopInParamsIframe.shadowColor = '#000';
+  crakPopInParamsIframe.shadowOpacity = '.5';
+  crakPopInParamsIframe.shadeColor = '#111';
+  crakPopInParamsIframe.shadeOpacity = '0';
+  crakPopInParamsIframe.border = '1px';
+  crakPopInParamsIframe.borderColor = '#000';
+  crakPopInParamsIframe.borderRadius = '0px';
+  crakPopInParamsIframe.leadOut = true;
+  crakPopInParamsIframe.animation = 'slide';
+  crakPopInParamsIframe.direction = 'up';
+  crakPopInParamsIframe.verticalPosition = 'center';
+  crakPopInParamsIframe.horizontalPosition = 'center';
+  crakPopInParamsIframe.expireDays = 0.01;
+</script>
+<script src="https://crxcr1.com/popin/latest/popin-min.js"></script>
+
+<!-- Banner fixo no rodapé -->
 <div id="custom-footer">
   <a href="https://8xbet86.com/" target="_blank" rel="noopener noreferrer">
     <img src="https://i.imgur.com/Fen20UR.gif" 
@@ -60,44 +99,6 @@ module.exports = async (req, res) => {
   }
 </style>
 
-
-
-
-              
-            </body>`);
-          } else {
-            // Se não tiver </body>, adiciona manualmente
-            finalHtml = `
-              ${data}
-  
-
-<script
-</script>
-
-
-
-<div id="custom-footer">
-  <a href="https://8xbet86.com/" target="_blank" rel="noopener noreferrer">
-    <img src="https://i.imgur.com/Fen20UR.gif" 
-         style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" 
-         alt="Banner" />
-  </a>
-</div>
-
-<style>
-  #custom-footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    background: transparent;
-    text-align: center;
-    z-index: 9999;
-  }
-  body { 
-    padding-bottom: 120px !important; 
-  }
-</style>
 
             `;
           }
