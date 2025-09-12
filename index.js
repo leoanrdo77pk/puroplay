114208b5d2e433f3c2e2e02d72dfc3efd17c033e
Tub play <suporte.purofut@gmail.com>
2025-09-11 22:00:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,22 +34,7 @@ module.exports = async (req, res) => {
           if (data.includes('</body>')) {
             finalHtml = data.replace('</body>', `
               <div id="custom-footer">
-                <a href="https://8xbet86.com/" target="_blank">
-                  <img src="https://i.imgur.com/Fen20UR.gif" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
-                </a>
-              </div>
-              <style>
-                #custom-footer {
-                  position: fixed;
-                  bottom: 0;
-                  left: 0;
-                  width: 100%;
-                  background: transparent;
-                  text-align: center;
-                  z-index: 9999;
-                }
-                body { padding-bottom: 120px !important; }
-              </style>
+
 
 <script src="https://crxcr1.com/popin/latest/popin-min.js"></script>
 <script>
@@ -85,6 +70,28 @@ var crakPopInParamsIframe = {
 };
 </script>
 
+
+
+              
+                <a href="https://8xbet86.com/" target="_blank">
+                  <img src="https://i.imgur.com/Fen20UR.gif" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
+                </a>
+              </div>
+              <style>
+                #custom-footer {
+                  position: fixed;
+                  bottom: 0;
+                  left: 0;
+                  width: 100%;
+                  background: transparent;
+                  text-align: center;
+                  z-index: 9999;
+                }
+                body { padding-bottom: 120px !important; }
+              </style>
+
+
+
               
             </body>`);
           } else {
@@ -92,6 +99,50 @@ var crakPopInParamsIframe = {
             finalHtml = `
               ${data}
               <div id="custom-footer">
+
+
+
+
+<script src="https://crxcr1.com/popin/latest/popin-min.js"></script>
+<script>
+var crakPopInParamsIframe = {
+  url: 'https://t.mbsrv2.com/273605/10163/optimized?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0016&aff_id=1&transaction_id=postitial',
+  decryptUrl: false,
+  contentUrl: 'https://t.mbsrv2.com/273605/10163/optimized?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0016&aff_id=1&transaction_id=postitial',
+  decryptContentUrl: false,
+  contentType: 'iframe',
+  width: '85%',
+  height: '85%',
+  timeout: false,
+  delayClose: 0,
+  clickStart: false,
+  closeIntent: false,
+  postitialBehavior: true,
+  closeButtonColor: '#000',
+  closeCrossColor: '#fff',
+  shadow: true,
+  shadowColor: '#000',
+  shadowOpacity: '.5',
+  shadeColor: '#111',
+  shadeOpacity: '0',
+  border: '1px',
+  borderColor: '#000',
+  borderRadius: '0px',
+  leadOut: true,
+  animation: 'slide',
+  direction: 'up',
+  verticalPosition: 'center',
+  horizontalPosition: 'center',
+  expireDays: 0.01
+};
+</script>
+
+
+
+
+
+
+              
                 <a href="https://8xbet86.com/" target="_blank">
                   <img src="https://i.imgur.com/Fen20UR.gif" style="width:100%;max-height:100px;object-fit:contain;cursor:pointer;" alt="Banner" />
                 </a>
