2a5dc01c0b2446e9e4333caf4396cf24cf124480
Tub play <suporte.purofut@gmail.com>
2025-09-11 21:56:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const https = require('https');
 module.exports = async (req, res) => {
   try {
     const path = req.url === '/' ? '' : req.url;
-    const targetUrl = 'https://startflix.vip/' + path;
+    const targetUrl = 'https://futebol7k.com/' + path;
 
     https.get(targetUrl, {
       headers: {
         'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
-        'Referer': 'https://startflix.vip/',
+        'Referer': 'https://futebol7k.com/',
       }
     }, (resp) => {
       let data = '';
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
         try {
           // Reescreve links para manter no domínio Vercel
           data = data
-            .replace(/https:\/\/startflix\.vip\//g, '/')
+            .replace(/https:\/\/futebol7k\.com\//g, '/')
             .replace(/href='\/([^']+)'/g, "href='/$1'")
             .replace(/href="\/([^"]+)"/g, 'href="/$1"')
             .replace(/action="\/([^"]+)"/g, 'action="/$1"')
